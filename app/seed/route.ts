4e5211312b5161777525dcc371fe8be6be03bd85
Timilehin Daramola <timi.homezone111@gmail.com>
2025-05-19 14:27:04 -0400

    latest commit

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+import bcrypt from 'bcrypt';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
